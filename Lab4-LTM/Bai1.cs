cb6a00c5b6ec17f3018d781e0caff2ad5cca880f
Huyen Tran <46748862+Meraviglioso8@users.noreply.github.com>
2023-05-09 20:31:31 +0700

    update lab 4

---
+++ Lab4-LTM/Bai1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4_LTM
+{
+    public partial class Bai1 : Form
+    {
+        public Bai1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = getHTTP(textBox1.Text);
+        }
+        private string getHTTP(string url)
+        {
+            WebRequest request;
+            WebResponse response;
+            string content = "";
+            Stream rs;
+            Byte[] RecvBytes = new byte[Byte.MaxValue];
+            Int32 bytes;
+            request = (WebRequest) WebRequest.Create(url);
+            response = (WebResponse)
+                request.GetResponse();
+            rs = response.GetResponseStream();
+            while (true)
+            {
+                bytes = rs.Read(RecvBytes, 0, RecvBytes.Length);
+                if (bytes <= 0) break;
+                content += System.Text.Encoding.UTF8.GetString(RecvBytes, 0, bytes);
+
+            }
+            return content;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            richTextBox1.Clear();
+        }
+    }
+}
---
+++ Lab4-LTM/Console.cs
@@ -0,0 +1,23 @@
+namespace Lab4_LTM
+{
+    public partial class Console : Form
+    {
+        public Console()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Bai1 b1 = new Bai1();
+            b1.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
