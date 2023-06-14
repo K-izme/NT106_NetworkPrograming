8d54523beba79fbb88e4bda7631edce917555d37
Kiet <kietngo255@gmail.com>
2023-06-15 00:34:15 +0700

    Online drawing

---
+++ Lab6-LTM/Lab6-LTM/Client.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Lab6_LTM
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            menuStrip1 = new MenuStrip();
+            menuToolStripMenuItem = new ToolStripMenuItem();
+            clearToolStripMenuItem = new ToolStripMenuItem();
+            quitToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Location = new Point(12, 52);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(645, 383);
+            pictureBox1.TabIndex = 13;
+            pictureBox1.TabStop = false;
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            pictureBox1.MouseUp += pictureBox1_MouseUp;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(676, 103);
+            label1.Name = "label1";
+            label1.Size = new Size(29, 15);
+            label1.TabIndex = 11;
+            label1.Text = "SIZE";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" });
+            comboBox1.Location = new Point(673, 137);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(115, 23);
+            comboBox1.TabIndex = 10;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(673, 215);
+            button2.Name = "button2";
+            button2.Size = new Size(115, 72);
+            button2.TabIndex = 9;
+            button2.Text = "Save";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(673, 412);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(115, 23);
+            textBox1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(673, 312);
+            button1.Name = "button1";
+            button1.Size = new Size(115, 72);
+            button1.TabIndex = 7;
+            button1.Text = "Color";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { menuToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 12;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            menuToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { clearToolStripMenuItem, quitToolStripMenuItem });
+            menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            menuToolStripMenuItem.Size = new Size(50, 20);
+            menuToolStripMenuItem.Text = "Menu";
+            // 
+            // clearToolStripMenuItem
+            // 
+            clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            clearToolStripMenuItem.Size = new Size(101, 22);
+            clearToolStripMenuItem.Text = "Clear";
+            // 
+            // quitToolStripMenuItem
+            // 
+            quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            quitToolStripMenuItem.Size = new Size(101, 22);
+            quitToolStripMenuItem.Text = "Quit";
+            // 
+            // Client
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(menuStrip1);
+            Name = "Client";
+            Text = "Client";
+            Load += Client_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private ComboBox comboBox1;
+        private Button button2;
+        private TextBox textBox1;
+        private Button button1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem menuToolStripMenuItem;
+        private ToolStripMenuItem clearToolStripMenuItem;
+        private ToolStripMenuItem quitToolStripMenuItem;
+    }
+}
---
+++ Lab6-LTM/Lab6-LTM/Client.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Lab6_LTM
+{
+    public partial class Client : Form
+    {
+        public Point end = new Point();
+        public Point start = new Point();
+        public Pen p;
+        bool draw = false;
+        string color;
+        Graphics graphics;
+        Bitmap bmp;
+        TcpClient client;
+        NetworkStream stream;
+        StreamWriter writer;
+        StreamReader reader;
+        Thread receiveThread;
+
+        public Client()
+        {
+            InitializeComponent();
+            GenerateVariables();
+        }
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void GenerateVariables()
+        {
+            graphics = pictureBox1.CreateGraphics();
+            comboBox1.Text = "8";
+            color = "#000000";
+            textBox1.Text = color;
+            CreateCanvas();
+
+            // Connect to the server
+            client = new TcpClient();
+            client.Connect("127.0.0", 1234); // Replace with your server IP address and desired port number
+
+            // Set up network stream and readers/writers
+            stream = client.GetStream();
+            writer = new StreamWriter(stream);
+            reader = new StreamReader(stream);
+
+            // Start a new thread to continuously receive drawing data from the server
+            receiveThread = new Thread(ReceiveDrawingData);
+            receiveThread.Start();
+        }
+
+        void CreateCanvas()
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(bmp);
+            pictureBox1.BackgroundImage = bmp;
+            pictureBox1.BackgroundImageLayout = ImageLayout.None;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            draw = true;
+
+            start = e.Location;
+
+            int size;
+
+            if (comboBox1.Text == "")
+            {
+                size = 8;
+            }
+            else
+            {
+                size = Convert.ToInt32(comboBox1.Text);
+            }
+
+            Color newColor = ColorTranslator.FromHtml(color);
+            p = new Pen(newColor, size);
+            p.SetLineCap(System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.DashCap.Round);
+
+            // Send the mouse down event to the server
+            writer.WriteLine($"MOUSE_DOWN|{start.X}|{start.Y}");
+            writer.Flush();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draw)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    end = e.Location;
+                    graphics.DrawLine(p, start, end);
+                    start = end;
+                    pictureBox1.Invalidate();
+
+                    // Send the mouse move event to the server
+                    writer.WriteLine($"MOUSE_MOVE|{end.X}|{end.Y}");
+                    writer.Flush();
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            draw = false;
+
+            // Send the mouse up event to the server
+            writer.WriteLine("MOUSE_UP");
+            writer.Flush();
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreateCanvas();
+
+            // Send a clear canvas event to the server
+            writer.WriteLine("CLEAR_CANVAS");
+            writer.Flush();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                color = "#" + (cd.Color.ToArgb() & 0x00FFFFFF).ToString("X6");
+                textBox1.Text = color;
+
+                // Send the color change event to the server
+                writer.WriteLine($"COLOR_CHANGE|{color}");
+                writer.Flush();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog();
+            s.Filter = "Png files| *.png|jpeg files| *.jpg|bitmaps | *.bmp";
+            if (s.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (File.Exists(s.FileName))
+                {
+                    File.Delete(s.FileName);
+                }
+                if (s.FileName.Contains(".jpg"))
+                {
+                    bmp.Save(s.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+                else if (s.FileName.Contains(".png"))
+                {
+                    bmp.Save(s.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                }
+                else if (s.FileName.Contains(".bmp"))
+                {
+                    bmp.Save(s.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                }
+            }
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Disconnect from the server and close the application
+            DisconnectFromServer();
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ReceiveDrawingData()
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    string data = reader.ReadLine();
+
+                    // Parse the received data and update the local canvas accordingly
+                    string[] tokens = data.Split('|');
+                    string eventType = tokens[0];
+
+                    if (eventType == "MOUSE_DOWN")
+                    {
+                        int x = int.Parse(tokens[1]);
+                        int y = int.Parse(tokens[2]);
+                        start = new Point(x, y);
+                    }
+                    else if (eventType == "MOUSE_MOVE")
+                    {
+                        int x = int.Parse(tokens[1]);
+                        int y = int.Parse(tokens[2]);
+                        end = new Point(x, y);
+                        graphics.DrawLine(p, start, end);
+                        start = end;
+                        pictureBox1.Invalidate();
+                    }
+                    else if (eventType == "MOUSE_UP")
+                    {
+                        draw = false;
+                    }
+                    else if (eventType == "CLEAR_CANVAS")
+                    {
+                        CreateCanvas();
+                    }
+                    else if (eventType == "COLOR_CHANGE")
+                    {
+                        color = tokens[1];
+                        textBox1.Text = color;
+                    }
+                }
+                catch (IOException)
+                {
+                    // An error occurred while reading data from the stream
+                    // Handle the error or gracefully terminate the application
+                    break;
+                }
+            }
+        }
+
+        private void DisconnectFromServer()
+        {
+            // Close all network resources
+            writer.Close();
+            reader.Close();
+            stream.Close();
+            client.Close();
+
+            // Join the receive thread (wait for it to finish) before closing the form
+            receiveThread.Join();
+        }
+    }
+}
